cbf1131f7440b379fe01ba07aec73775c4afa3c8
Alvaro <alvarof260@gmail.com>
2023-05-20 16:37:34 -0300

    feat: nuevo feature CartWidget

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,17 @@
 import { Categories } from "../Categories/Categories"
-
+import { CartWidget } from "../CartWidget/CartWidget"
 export function NavBar(){
     const menuList = ['Computadoras','Notebooks','Accesorios']
     return(
-        <nav className="navbar bg-platinum py-2 flex justify-evenly">
-            <h1 className="text-2xl font-bold text-gray-700">
-                <span className="text-mint">M</span>undo<span className="text-mint">G</span>amer.
-            </h1>
-
-            <Categories items={menuList}/>
-            
+        <nav className="navbar py-2 flex justify-evenly">
+            <div className="flex gap-2">
+                <h1 className="text-2xl font-bold text-gray-700">
+                    <span className="text-mint">M</span>undo<span className="text-mint">G</span>amer.
+                </h1>
+                
+                <Categories items={menuList}/>
+            </div>
+            <CartWidget/>
         </nav>
     )
 }
