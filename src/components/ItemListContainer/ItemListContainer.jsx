4ce8fab0b5e1436859b69b75e164da10b1cdeda3
Alvaro <alvarof260@gmail.com>
2023-05-30 14:23:16 -0300

    cleanup: borrar mock viejos y añadir nuevos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import { getProducts } from "../../Mocks/asyncMocks";
+import { Item } from "../Item/Item"
 export function ItemListContainer({ greeting }) {
   const [products, setproducts] = useState([]);
   useEffect(() => {
@@ -12,6 +13,8 @@ export function ItemListContainer({ greeting }) {
       <h2 className="font-bold text-4xl h-screen flex justify-center items-center">
         {greeting}
       </h2>
+      {products.map(product=><Item key={product.id} {...product}/>)}
+      
     </article>
   );
 }
