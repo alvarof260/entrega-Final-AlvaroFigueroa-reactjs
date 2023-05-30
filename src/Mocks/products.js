b329c1d57514bd01e2714860e0ec71bc4cd1fb82
Alvaro <alvarof260@gmail.com>
2023-05-30 11:07:52 -0300

    mock: productos cargadas con funcion async

---
+++ src/Mocks/products.js
@@ -0,0 +1,281 @@
+const products = [
+  {
+    id: "NOT2396",
+    name: 'NOTEBOOK LENOVO 14" IDEAPAD 3 CELERON N4120 4GB 256GB W11H',
+    category: "notebook",
+    description: `PERFORMANCE
+        Processor Intel® Celeron® N4120 (4C / 4T, 1.1 / 2.6GHz, 4MB)
+        Graphics Integrated Intel® UHD Graphics 600
+        Chipset Intel® SoC Platform
+        Memory 4GB Soldered DDR4-2400
+        Memory Slots Memory soldered to systemboard, no slots
+        Max Memory 4GB soldered memory, not upgradable
+        Storage 256GB SSD M.2 2242 PCIe® 3.0x2 NVMe®
+        Storage Support Up to two drives, 1x 2.5" HDD + 1x M.2 SSD
+        • 2.5" HDD up to 1TB
+        • M.2 2242 SSD up to 512GB
+        • M.2 2280 SSD up to 512GB
+        Card Reader 4-in-1 Card Reader
+        Optical None
+        Audio Chip High Definition (HD) Audio
+        Speakers Stereo speakers, 1.5W x2, Dolby® Audio™
+        Camera 0.3MP with Privacy Shutter
+        Microphone 2x, Array
+        Battery Integrated 35Wh
+        Max Battery Life MobileMark® 2014: 14 hr
+        Power Adapter 45W Round Tip (2-pin, Wall-mount)
+        
+        DESIGN
+        Display 14" HD (1366x768) TN 220nits Anti-glare
+        Touchscreen None
+        Keyboard Non-backlit, English
+        Case Color Platinum Grey
+        Surface Treatment IMR (In-Mold Decoration by Roller)
+        Case Material PC-ABS (Top), PC-ABS (Bottom)
+        Dimensions (WxDxH) 327.1 x 241 x 19.9 mm
+        Weight Starting at 1.5 kg
+        
+        SOFTWARE
+        Operating System Windows® 11 Home
+        Bundled Software Office Trial
+        
+        CONNECTIVITY
+        Ethernet No Onboard Ethernet
+        WLAN + Bluetooth 11ac 2x2 + BT5.0
+        Standard Ports
+        1x USB 2.0
+        2x USB 3.2 Gen 1
+        1x HDMI® 1.4b
+        1x Card reader
+        1x Headphone / microphone combo jack (3.5mm)
+        1x Power connector
+        
+        SECURITY & PRIVACY
+        Security Chip Firmware TPM 2.0
+        Fingerprint Reader None
+        Other Security Camera privacy shutter`,
+    price: "184_900",
+    currentPrice: "ARS",
+    stock: 7,
+  },
+  {
+    id: "NOT1495",
+    name: 'NOTEBOOK ASUS 15.6" X515EA I3-1115G4 4GB 256GB FHD FREE',
+    category: "notebook",
+    description: `Pantalla 15.6" FULLHD (1920 x 1080)
+    Tecnología de display LED
+    Camara Web Si
+    
+    Especificaciones
+    Procesador Intel Core i3
+    Caché 6 MB
+    Velocidad de Reloj 3.0 Ghz
+    Modelo de Procesador 1115G4
+    Generación 11va
+    
+    Especificaciones
+    Memoria
+    Capacidad Memoria 4GB
+    Tecnología de memoria DDR4
+    Formato de memoria OnBoard / SODIMM
+    
+    Especificaciones
+    Almacenamiento
+    Capacidad de Almacenamiento SSD 256GB
+    Formato de almacenamiento M.2 NVME PCI-e
+    
+    Especificaciones
+    Video
+    Placa de Video Integrada
+    
+    Especificaciones
+    Conectividad
+    Tecnología de conectividad Wi-Fi Si
+    Bluetooth 4.1
+    
+    Especificaciones
+    Puertos
+    Puertos Externos HDMI 1.4, USB 3.2 Gen 1 (x1), 3.5mm Combo Audio Jack//2x USB 2.0 Type-A
+    
+    Especificaciones
+    Apariencia
+    Dimensiones y peso del producto 1,80Kg
+    
+    Especificaciones
+    Batería
+    Autonomía 37hs
+    
+    Especificaciones
+    Fuente de alimentación
+    Potencia 45W
+    Tipo de Fuente Externa`,
+    price: "248_800",
+    currentPrice: "ARS",
+    stock: 3,
+  },
+  {
+    id: "",
+    name: 'NOTEBOOK DELL 15.6" INSPIRON 3525 RYZEN 5 AMD R5 5625U 8GB 256GB FHD W1',
+    category: "notebook",
+    description: `Sistema Operativo Microsoft Windows 11
+    PorcesadorAMD Ryzen 5 5625U
+    Memoria 8GB DDR4 3200Mhz Non-ECC
+    Almacenamiento 256GB SSD
+    Unidad óptica No incluye
+    Pantalla 15.6" FHD (1920 x 1080)
+    Gráficos AMD® Radeon Graphics
+    Cámara Si`,
+    price: "339_000",
+    currentPrice: "ARS",
+    stock: 4,
+  },
+  {
+    id: "NOT2397",
+    name: 'NOTEBOOK LENOVO 15.6" THINKBOOK I5-1135G7 8GB 256GB FREE',
+    category: "notebook",
+    description: `Modelo: ThinkBook 15 G2 ITL
+    Número de parte: 20VE00L4AR
+    
+    RENDIMIENTO
+    Procesador Intel Core i5-1135G7 (4C/8T, 2,4/4,2 GHz, 8 MB)
+    Gráficos Intel UHD Graphics
+    Chipset Plataforma Intel SoC
+    Memoria DDR4-3200 soldado de 8GB
+    Ranuras de memoria Una memoria soldada a la placa del sistema, una ranura DDR4 SO-DIMM, compatible con dos canales
+    Memoria máxima Hasta 40 GB (8 GB soldados + 32 GB SO-DIMM) DDR4-3200
+    Almacenamiento SSD de 256 GB M.2 2242 PCIe 3.0×4 NVMe + bahía de disco duro vacía
+    Soporte de almacenamiento Modo HDD/SSD: hasta dos unidades, 1 HDD/SSD de 2,5" + 1 SSD M.2
+    Lector de tarjetas 4 en 1
+    Óptico: Ninguna
+    Microprocesador de audio Audio de alta definición (HD), códec Realtek ALC3287
+    Altavoces estéreo, 2W x2, Dolby Audio
+    Cámara 720p con ThinkShutter
+    Micrófono 2x, matriz
+    Batería Integrado 45Wh
+    Duración máxima de la batería: MobileMark 2018: 6 h (45 Wh)
+    Adaptador de corriente USB-C de 65 W (3 pines)
+    
+    DISEÑO
+    Pantalla FHD de 15,6" (1920 x 1080) TN 220 nits Antirreflejo, 45 % NTSC
+    Pantalla táctil: No
+    Teclado Sin retroiluminación, español (LA)
+    Color de la caja gris mineral
+    Tratamiento de superficies Chorro de arena anodizado
+    Material de la Caja Aluminio (Superior), PC + ABS (Inferior)
+    Dimensiones (An. x Pr. x Al.) 357 x 235 x 18,9 mm
+    Peso 1,7 kg
+    
+    SOFTWARE
+    Sistema operativo Ninguno
+    Software incluido Ninguno
+    
+    CONECTIVIDAD
+    Ethernet 100/1000M
+    WiFi + Bluetooth 11ac, 2×2 + BT5.0
+    Puertos estándar
+    
+    • 1x USB 3.2 Gen 1
+    • 1x USB 3.2 Gen 1 (siempre encendido)
+    • 1x USB-C 3.2 Gen 2 (admite transferencia de datos, Power Delivery 3.0 y DisplayPort 1.4)
+    • 1x Thunderbolt 4 / USB4 40Gbps (admite transferencia de datos, Power Delivery 3.0 y DisplayPort 1.4)
+    • 1xHDMI 1.4b
+    • 1x lector de tarjetas
+    • 1x Ethernet (RJ-45)
+    • 1x Conector combinado de auriculares/micrófono (3,5 mm)
+    
+    SEGURIDAD Y PRIVACIDAD
+    Chip de seguridad Firmware TPM 2.0
+    Lector de huellas dactilares Estilo táctil
+    Cerraduras físicas: Ranura de seguridad Kensington Nano
+    Otra seguridad: Cubierta de la cámara ThinkShutter
+    
+    SERVICIO
+    Garantía básica 1 año, depósito
+    
+    CERTIFICACIONES
+    Certificaciones Verdes
+    
+    • EPEAT SILVER
+    • ENERGY STAR 8.0
+    • ErP Lote 3
+    • RoHS`,
+    price: "549.865",
+    currentPrice: "ARS",
+    stock: 10,
+  },
+  {
+    id: "NOT1825",
+    name: 'NOTEBOOK ASUS 15.6" ROG RYZEN 7 R7 6800H RTX 3060 16GB 512GB AMD R7 W11H',
+    category: "gamer",
+    description: `Procesador AMD Ryzen 7
+    Modelo procesador AMD Ryzen™ 7 6800H Mobile Processor (8-core/16-thread, 20MB cache, up to 4.7 GHz max boost)
+    Almacenamiento SSD 512GB
+    Memoria RAM 16GB
+    Sistema Operativo Windows 11 Home
+    
+    Imagen
+    Tamaño Pantalla 15.6"
+    Resolución de pantalla 2560 x 1440 QHD
+    
+    Conectividad
+    Wi-Fi Sí
+    Puertos USB 4
+    Puerto HDMI 1
+    Bluetooth Sí
+    
+    Detalles
+    WEBCam Sí
+    Color Negro
+    
+    Dimensiones
+    Medidas 35.4 x 25.9 x 2.26 cm
+    Peso 2.30 Kg`,
+    price: "1_138_833",
+    currentPrice: "ARS",
+    stock: 5,
+  },
+  {
+    id: "NOT2132",
+    name: 'NOTEBOOK GIGABYTE 15.6" AORUS 15 XE4 I7-12700H 16GB 1TB RTX 3070 TI 165HZ W11H',
+    category: "gamer",
+    description: `Pantalla: 15.6"  2560×1440 - 165HZ
+    Procesador: Intel Core i7 12700H
+    Memoria: 16GB DDR4 3200 MHz
+    Gráficos: NVIDIA GeForce RTX 3070 TI 8GB
+    SSD: 1TB NVME PCIE 4.0
+    Sistema Operativo: Windows 11 Home
+    Conectividad: Intel Wi-Fi 6E AX210 (Gig+), Bluetooth v5.2
+    Left side:1 x HDMI 2.11 x USB 3.2 Gen1 (Type-C with DP)1 x Audio combo jack1 x USB 3.2 Gen1 (Type-A)Right side:1 x DC input1 x Thunderbolt™ 4 (Type-C)1 x RJ45`,
+    price: "1_257_264",
+    currentPrice: "ARS",
+    stock: 1,
+  },
+  {
+    id: "NOT2263",
+    name: 'NOTEBOOK ASUS 14.5" ZENBOOK PRO DUO I7-12700H 16GB 1TB DUAL SCREEN W11H',
+    category: "notebook",
+    description: `Sistema Operativo: Windows 11 Home 
+    Procesador: Intel® Core™ i7-12700H Processor 2.3 GHz (24M Cache, up to 4.7 GHz, 6P+8E cores)
+    Tarjeta Gráfica: Intel Iris Xe Graphics
+    Memoria RAM: 16GB LPDDR5 
+    Almacenamiento: 1TB M.2 NVMe™ PCIe® 4.0 Performance SSD
+    Pantalla: 2.8K (2880 x 1800) OLED 16:10 aspect ratio
+    Pantalla adicional: ScreenPad™ Plus (12.7" 2880 x 864 IPS-level Panel Support Stylus)
+    Teclado: Retroiluminado
+    Color: Negro
+    Tecnologías: Wifi 6, Grado militar.`,
+    price: "1_342_392",
+    currentPrice: "ARS",
+    stock: 6,
+  },
+];
+
+const DELAY = 300;
+
+export const getProducts = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products);
+    }, DELAY);
+  });
+};
