c91054e916c5a71907a7755a7d6cc47f0f7f3d53
Alvaro <alvarof260@gmail.com>
2023-06-07 09:12:06 -0300

    feat: agregar funcionalidad a card navegable por su id

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,19 @@
 import { ItemPreview } from "../ItemPreview/ItemPreview";
 import { ItemCount } from "../ItemCount/ItemCount";
 import { Price } from "../Price/Price";
+import { Link } from "react-router-dom";
 
 export const Item = (props) => {
-  const {images,name,stock, price, currentPrice,} = props
+  const {images,name,stock, price, currentPrice, id} = props
   
   return (
-    <a className="border-slate-300 bg-white border rounded p-2 max-w-[340px] max-h-[500px] cursor-pointer">
+    <Link to={`/${id}`} className="border-slate-300 bg-white border rounded p-2 max-w-[340px] max-h-[500px] cursor-pointer">
       <ItemPreview className="w-[320px]" images={images}/>
       <div className="flex gap-4 flex-col">
         <h2 title={name} className="text-black font-bold pb-4 h-[26px] overflow-hidden">{name}</h2>
         <Price price={price} currentPrice={currentPrice}/>
         <ItemCount initial={1} stock={stock} onAdd={(quantity)=> console.log(`Cantidad agregada:${quantity}`)}/>
       </div>
-    </a>
+    </Link>
   );
 };
