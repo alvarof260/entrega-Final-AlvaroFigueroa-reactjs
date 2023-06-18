0c8d55e414cb8df93b8959ba1b309bdfebd2f7f9
Alvaro <alvarof260@gmail.com>
2023-06-18 02:07:59 -0300

    fix: Item count ahora tiene un estado al agregar productos

--- src/components/detail/ItemDetail/ItemDetail.jsx
+++ src/components/detail/ItemDetail/ItemDetail.jsx
@@ -2,6 +2,9 @@ import { ItemPreview } from "../../store/ItemPreview/ItemPreview";
 import { ItemInfo } from "../ItemInfo/ItemInfo";
 import { ProductDescription } from "../ProductDescription/ProductDescription";
 import { ItemCount } from "../ItemCount/ItemCount";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IconCart } from "../../../Icons/IconCart"
 
 export const ItemDetail = ({
   name,
@@ -13,6 +16,8 @@ export const ItemDetail = ({
   currentPrice,
   stock,
 }) => {
+  const [quantityAdded, setQuantityAdded] = useState(0);
+  const handleQuantityAdded = (quantity) => setQuantityAdded(quantity);
   return (
     <article className="h-screen w-screen px-60 flex flex-row gap-4 justify-center items-center">
       <ItemPreview className="w-[600px] rounded" images={images} />
@@ -26,11 +31,18 @@ export const ItemDetail = ({
           brand={brand}
         />
         <ProductDescription description={description} />
-        <ItemCount
-          initial={1}
-          stock={stock}
-          onAdd={(quantity) => console.log(`Cantidad agregada:${quantity}`)}
-        />
+        {quantityAdded > 0 ? (
+          <Link
+            to={"/cart"}
+            className={
+              "border rounded p-2 w-full flex justify-center items-center gap-1 hover:bg-mint hover:text-white  transition-all ease-linear duration-500"
+            }
+          >
+            Ir al Carrito <IconCart/>
+          </Link>
+        ) : (
+          <ItemCount initial={1} stock={stock} onAdd={handleQuantityAdded} />
+        )}
       </div>
     </article>
   );
