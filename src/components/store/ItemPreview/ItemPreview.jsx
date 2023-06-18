7ebba9b84ceb71b3c1cc972244b6e00282311f41
Alvaro <alvarof260@gmail.com>
2023-06-18 12:47:15 -0300

    fix: agrgar animacion ItemPreview

--- src/components/store/ItemPreview/ItemPreview.jsx
+++ src/components/store/ItemPreview/ItemPreview.jsx
@@ -8,19 +8,19 @@ export const ItemPreview = ({ images, className, alt = "", title }) => {
   const handleMouseLeave = () => setIsHovered(false);
 
   return (
-    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {!isHovered ? <ProductThumbnail 
-        className={className}
+    <div className={className} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <ProductThumbnail 
+        className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${isHovered ? 'opacity-0' : 'opacity-100'}`}
         images={images[0]}
         alt={alt}
         title={title}
-      /> : 
+      /> 
       <ProductThumbnail 
-        className={className}
+        className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${isHovered ? 'opacity-100' : 'opacity-0'}`}
         images={images[1]}
         alt={alt}
         title={title}
-      />}
+      />
     </div>
   );
 };
