c314976bb3cc9eced6f09e5ccfb71704f6d72587
Alvaro <alvarof260@gmail.com>
2023-06-18 19:00:41 -0300

    cleanup & fix: borrar dependecias sin uso y error de imagen

--- src/components/detail/ItemDetail/ItemDetail.jsx
+++ src/components/detail/ItemDetail/ItemDetail.jsx
@@ -1,10 +1,10 @@
-import { ItemPreview } from "../../store/ItemPreview/ItemPreview";
 import { ItemInfo } from "../ItemInfo/ItemInfo";
 import { ProductDescription } from "../ProductDescription/ProductDescription";
 import { ItemCount } from "../ItemCount/ItemCount";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { IconCart } from "../../../Icons/IconCart"
+import { ProductThumbnail } from "../ProductThumbnail/ProductThumbnail"
 
 export const ItemDetail = ({
   name,
@@ -20,7 +20,7 @@ export const ItemDetail = ({
   const handleQuantityAdded = (quantity) => setQuantityAdded(quantity);
   return (
     <article className="h-screen w-screen px-60 flex flex-row gap-4 justify-center items-center">
-      <ItemPreview className="w-[600px] rounded" images={images} />
+      <ProductThumbnail className="w-[600px] rounded" images={images[0]} />
       <div className="flex flex-col justify-center gap-8">
         <ItemInfo
           name={name}
