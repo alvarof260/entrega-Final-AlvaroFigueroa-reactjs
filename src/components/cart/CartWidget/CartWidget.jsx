a3c1b1080668ffbd9793e42da3dda0ad1fbd4257
Alvaro <alvarof260@gmail.com>
2023-07-06 21:47:33 -0300

    style: cambio de boton CartWidget

--- src/components/cart/CartWidget/CartWidget.jsx
+++ src/components/cart/CartWidget/CartWidget.jsx
@@ -8,7 +8,7 @@ export function CartWidget() {
   return (
     <>
       <Link to={'/cart'} className="flex flex-grow basis-0 justify-end">
-        <div className="hover:bg-slate-300 rounded-full flex gap-2 p-1">
+        <div className="rounded-full flex gap-2 p-1 hover:bg-gray-500/10 transition-all duration-700 ease-in-out">
           <IconCart/>
           <span className="bg-mint rounded-full w-6 h-6 text-center text-gray-50">{countItemsInCart()}</span>
         </div>
