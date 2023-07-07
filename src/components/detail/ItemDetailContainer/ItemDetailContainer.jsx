4cfc0a248e355e90ef7468cc6ea6c2f8272003aa
Alvaro <alvarof260@gmail.com>
2023-07-07 17:48:04 -0300

    stash: error en itemDetailContainer data error

--- src/components/detail/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/detail/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,19 +1,28 @@
 import { useState, useEffect } from "react";
-import { getProductById } from "../../../Mocks/asyncMocks";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../../firebase/firebaseconfig";
 
 export const ItemDetailContainer = () => {
   const [product, setProduct] = useState(null);
-  const {productId} = useParams()
+  const { productId } = useParams();
 
   useEffect(() => {
+    const itemIdRef = doc(db, "Items", productId);
+    getDoc(itemIdRef).then((response) => {
+      const newItem = {
+        id: response.id,
+        ...response.data(),
+      };
+      setProduct(newItem);
+    });
     /*getProductById(productId)
       .then((response) => setProduct(response))*/
   }, [productId]);
   return (
     <section>
-      <ItemDetail  {...product} />
+      <ItemDetail {...product} />
     </section>
   );
 };
